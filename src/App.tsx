4e63b538df5dc4bee0932901bae54b6356fdd3dd
Karim Yassine <karimwebdevelopment@gmail.com>
2022-06-16 19:51:23 -0300

    feat: 🎸 add theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,35 @@
+import { ThemeProvider } from "@emotion/react";
+
+const theme = {
+  colors: {
+    white: 0xffffff,
+    black: 0x000112,
+    purple: {
+      primary: 0x635fc7,
+      secondary: 0xa8a4ff,
+    },
+    red: {
+      primary: 0xea5555,
+      secondary: 0xff9898,
+    },
+    dark: {
+      primary: 0x20212c,
+      secondary: 0x2b2c37,
+      tertiary: 0x3e3f4e,
+    },
+    light: {
+      primary: 0x828fa3,
+      secondary: 0xe4ebfa,
+      tertiary: 0xf4f7fd,
+    },
+  },
+};
+
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'></header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <></>
+    </ThemeProvider>
   );
 }
 
