6589abc5c58eb0835700bcc6ef38f7470d3e73e9
Karim Yassine <karimwebdevelopment@gmail.com>
2022-06-16 19:59:08 -0300

    refactor: 💡 change theme position

---
+++ src/utils/globals.ts
@@ -0,0 +1,24 @@
+export const theme = {
+  colors: {
+    white: 0xffffff,
+    black: 0x000112,
+    purple: {
+      primary: 0x635fc7,
+      secondary: 0xa8a4ff,
+    },
+    red: {
+      primary: 0xea5555,
+      secondary: 0xff9898,
+    },
+    dark: {
+      primary: 0x20212c,
+      secondary: 0x2b2c37,
+      tertiary: 0x3e3f4e,
+    },
+    light: {
+      primary: 0x828fa3,
+      secondary: 0xe4ebfa,
+      tertiary: 0xf4f7fd,
+    },
+  },
+};
