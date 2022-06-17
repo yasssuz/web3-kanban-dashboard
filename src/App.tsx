ceb09d28088d2753984613d04b403b465c7ffe0a
Karim Yassine <karimwebdevelopment@gmail.com>
2022-06-16 21:02:11 -0300

    feat: 🎸 add dashboard component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Global } from "@emotion/react";
 import { globalStyles } from "./utils/globals";
+import Dashboard from "./components/dashboard";
 
 function App() {
   return (
     <>
       <Global styles={globalStyles} />
+      <Dashboard />
     </>
   );
 }
