22be43125649044717d83efe91c45f1d6cb544f2
Karim Yassine <karimwebdevelopment@gmail.com>
2022-06-16 23:21:25 -0300

    refactor: 💡 change color formats

--- src/utils/globals.ts
+++ src/utils/globals.ts
@@ -2,23 +2,23 @@ import { css } from "@emotion/react";
 
 export const globalStyles = css`
   :root {
-    --white: 0xffffff;
-    --black: 0x000112;
-    --purplePrimary: 0x635fc7;
-    --purpleSecondary: 0xa8a4ff;
-    --redPrimary: 0xea5555;
-    --redSecondary: 0xff9898;
-    --text: 0x000112;
-    --line: 0xe4ebfa;
-    --primaryBg: 0xffffff;
-    --secondaryBg: 0xf4f7fd;
+    --white: #ffffff;
+    --black: #000112;
+    --purplePrimary: #635fc7;
+    --purpleSecondary: #a8a4ff;
+    --redPrimary: #ea5555;
+    --redSecondary: #ff9898;
+    --text: #000112;
+    --line: #e4ebfa;
+    --primaryBg: #ffffff;
+    --secondaryBg: #f4f7fd;
   }
 
   [data-theme="dark"] {
-    --text: 0xffffff;
-    --line: 0x3e3f4e;
-    --primaryBg: 0x2b2c37;
-    --secondaryBg: 0x20212c;
+    --text: #ffffff;
+    --line: #3e3f4e;
+    --primaryBg: #2b2c37;
+    --secondaryBg: #20212c;
   }
 
   *,
