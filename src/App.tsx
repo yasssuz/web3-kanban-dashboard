f6d7d6f614036d7147527da184aa5baa77c571f9
Karim Yassine <karimwebdevelopment@gmail.com>
2022-06-17 16:12:50 -0300

    feat: 🎸 setup router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import { Global } from "@emotion/react";
 import { globalStyles } from "./utils/globals";
 import Dashboard from "./components/dashboard";
+import { Link, Route, Routes } from "react-router-dom";
+import BaseLayout from "./layouts/base-layout";
 
 function App() {
   return (
     <>
       <Global styles={globalStyles} />
-      <Dashboard />
+      <BaseLayout>
+        <Routes>
+          {/* <Route path="/login" element={<Login />} /> */}
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/dashboard/:dashboardName' element={<Dashboard />} />
+          <Route path='*' element={<h1>nothing here</h1>} />
+        </Routes>
+      </BaseLayout>
     </>
   );
 }
