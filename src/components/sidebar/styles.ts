02de573b596b932c68732645d6f0b5e0d428cfd6
Karim Yassine <karimwebdevelopment@gmail.com>
2022-06-19 10:49:29 -0300

    style: 💄 add animations

--- src/components/sidebar/styles.ts
+++ src/components/sidebar/styles.ts
@@ -1,6 +1,6 @@
 import styled from "@emotion/styled";
 
-export const OpenSidebarButton = styled.button`
+export const OpenSidebarButton = styled.button<{ isSidebarOpen: boolean }>`
   background: var(--purplePrimary);
   border-radius: 0px 100px 100px 0px;
   width: 56px;
@@ -9,27 +9,44 @@ export const OpenSidebarButton = styled.button`
   place-content: center;
   border: 0;
   position: fixed;
-  left: 0;
+  left: ${props => (props.isSidebarOpen ? "-56px" : "0px")};
   bottom: 32px;
-  transition: background-color 0.3s ease-out;
+  transition: left 0.3s cubic-bezier(0.53, 0.21, 0, 1);
+  transition-delay: ${props => (props.isSidebarOpen ? "0s" : "0.4s")};
   cursor: pointer;
+  overflow: hidden;
 
-  &:hover {
+  &:hover::before {
+    transform: scaleX(1);
+    transform-origin: left;
+  }
+
+  &::before {
+    content: "";
+    position: absolute;
+    height: 48px;
+    width: 100%;
     background: var(--purpleSecondary);
+    z-index: -1;
+    transform: scaleX(0);
+    transform-origin: right;
+    transition: transform 0.6s cubic-bezier(0.53, 0.21, 0, 1);
   }
 `;
 
-export const SidebarContainer = styled.aside`
+export const SidebarContainer = styled.aside<{ isSidebarOpen: boolean }>`
   position: fixed;
   width: 300px;
-  left: 0;
   height: 100%;
+  left: ${props => (props.isSidebarOpen ? "0px" : "-300px")};
   background: var(--secondaryBg);
   border-right: 1px solid var(--line);
   display: flex;
   flex-direction: column;
   justify-content: space-between;
   padding: 15px 0 47px;
+  transition: left 0.5s cubic-bezier(0.53, 0.21, 0, 1);
+  transition-delay: ${props => (props.isSidebarOpen ? "0.25s" : "0s")};
 `;
 
 export const TopArea = styled.div`
@@ -55,10 +72,7 @@ export const Board = styled.li`
     gap: 16px;
     padding: 14px 32px;
     transition: background-color 0.3s ease-out;
-
-    &:hover {
-      background: rgba(99, 95, 199, 0.1);
-    }
+    overflow: hidden;
 
     span {
       transition: color 0.3s ease;
@@ -75,6 +89,24 @@ export const Board = styled.li`
     left: -100%;
     z-index: -1;
     transition: left 0.3s ease;
+  }
+
+  a:hover::after {
+    transform: scaleX(1);
+    transform-origin: left;
+  }
+
+  a::after {
+    content: "";
+    position: absolute;
+    height: 100%;
+    width: 100%;
+    left: 0;
+    background: rgba(99, 95, 199, 0.1);
+    z-index: -1;
+    transform: scaleX(0);
+    transform-origin: right;
+    transition: transform 0.6s cubic-bezier(0.53, 0.21, 0, 1);
   }
 
   &.selected a {
