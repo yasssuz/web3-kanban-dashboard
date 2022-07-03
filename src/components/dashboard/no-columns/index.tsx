81354b676ed0755939b5ee995d79780c736e86ff
Karim Yassine <karimwebdevelopment@gmail.com>
2022-07-03 13:09:55 -0300

    feat: 🎸 add modal

--- src/components/dashboard/no-columns/index.tsx
+++ src/components/dashboard/no-columns/index.tsx
@@ -1,18 +1,32 @@
+import { useState } from "react";
+import EditOrCreateBoardModalState from "../../modal-states/edit-or-create-board";
 import Button from "../../shared/buttons";
+import Modal from "../../shared/modal";
 import Heading from "../../shared/typography/heading";
-import Text from "../../shared/typography/text";
 import { Container } from "./styles";
 
 function NoColumns() {
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+
   return (
-    <Container>
-      <Heading as='h2' size='medium'>
-        This board is empty. Create a new column to get started.
-      </Heading>
-      <Button variant='primary' size='large' width='174px'>
-        + Add New Column
-      </Button>
-    </Container>
+    <>
+      <Container>
+        <Heading as='h2' size='medium'>
+          This board is empty. Create a new column to get started.
+        </Heading>
+        <Button
+          variant='primary'
+          size='large'
+          width='174px'
+          onClick={() => setIsModalOpen(true)}
+        >
+          + Add New Column
+        </Button>
+      </Container>
+      <Modal isOpen={isModalOpen}>
+        <EditOrCreateBoardModalState edit />
+      </Modal>
+    </>
   );
 }
 
