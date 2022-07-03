cc19f448bebea2c9ff5ef5a71da33fc7e88ad1a4
Karim Yassine <karimwebdevelopment@gmail.com>
2022-07-03 13:09:31 -0300

    feat: 🎸 add styles

---
+++ src/components/dashboard/styles.ts
@@ -0,0 +1,22 @@
+import { css } from "@emotion/react";
+import styled from "@emotion/styled";
+import { MediaQuery } from "../../utils/globals";
+
+export const DashboardContainer = styled.main<{ isCentralized: boolean }>`
+  display: grid;
+  min-height: calc(100vh - 96px);
+
+  ${MediaQuery.md} {
+    min-height: calc(100vh - 81px);
+  }
+
+  ${MediaQuery.sm} {
+    min-height: calc(100vh - 64px);
+  }
+
+  ${props =>
+    props.isCentralized &&
+    css`
+      place-content: center;
+    `}
+`;
