7e8cfe5032ff29445f9eafdbdd00f1a15c86c22d
Karim Yassine <karimwebdevelopment@gmail.com>
2022-07-03 13:09:13 -0300

    feat: 🎸 make component dynamic depending on state

--- src/components/dashboard/index.tsx
+++ src/components/dashboard/index.tsx
@@ -1,8 +1,19 @@
-function Dashboard() {
+import NoColumns from "./no-columns";
+import { DashboardContainer } from "./styles";
+
+interface DashboardProps {
+  index?: boolean;
+}
+
+function Dashboard({ index }: DashboardProps) {
+  const columns = [];
+
   return (
-    <main>
-      <h1>dashboard</h1>
-    </main>
+    <DashboardContainer isCentralized={!columns.length || !!index}>
+      {(index && <h1>index</h1>) || (!columns.length && <NoColumns />) || (
+        <h1>has colums</h1>
+      )}
+    </DashboardContainer>
   );
 }
 
