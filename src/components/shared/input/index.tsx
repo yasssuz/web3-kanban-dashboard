824f04be143e8f2397e124b3390a4773693b5fd6
Karim Yassine <karimwebdevelopment@gmail.com>
2022-07-24 16:58:28 -0300

    add forwardRef

--- src/components/shared/input/index.tsx
+++ src/components/shared/input/index.tsx
@@ -1,4 +1,4 @@
-import { DetailedHTMLProps, InputHTMLAttributes } from "react";
+import { DetailedHTMLProps, forwardRef, InputHTMLAttributes, Ref } from "react";
 import { InputContainer, InputStyles, ErrorMessage, TopLabel } from "./styles";
 
 export interface InputProps
@@ -12,14 +12,17 @@ export interface InputProps
   error?: string;
 }
 
-function Input({ error, label, id, ...rest }: InputProps) {
+function Input(
+  { error, label, id, ...rest }: InputProps,
+  ref: Ref<HTMLInputElement>
+) {
   return (
     <InputContainer>
       <TopLabel htmlFor={id}>{label}</TopLabel>
-      <InputStyles error={error} id={id} {...rest} />
+      <InputStyles error={error} id={id} ref={ref} {...rest} />
       {error && <ErrorMessage htmlFor={id}>{error}</ErrorMessage>}
     </InputContainer>
   );
 }
 
-export default Input;
+export default forwardRef(Input);
