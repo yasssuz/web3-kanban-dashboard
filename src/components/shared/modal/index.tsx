a7937f9e6840f5ba44205be2b0cc60327ba12329
Karim Yassine <karimwebdevelopment@gmail.com>
2022-07-24 16:58:17 -0300

    remove useless import

--- src/components/shared/modal/index.tsx
+++ src/components/shared/modal/index.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useState } from "react";
+import { ReactNode } from "react";
 import { ModalContainer, Overflow } from "./styles";
 import { LazyMotion, domAnimation, AnimatePresence } from "framer-motion";
 
